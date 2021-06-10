4fa05dfc68789e18e8a03c404249f6e5b9729535
张夜白 <zhangyebai@126.com>
2021-06-10 14:36:44 +0800

    fix-new gitignore

--- toc-web/src/main/java/com/zyb/toc/web/TocWebApplication.java
+++ toc-web/src/main/java/com/zyb/toc/web/TocWebApplication.java
@@ -2,7 +2,9 @@ package com.zyb.toc.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -14,9 +16,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @date 2021/6/7 2:07 PM
  * @version 1.0.0
  **/
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = "com.galaxy.toc")
+@EnableFeignClients(basePackages = "com.zyb.toc")
+@EnableEurekaClient
 public class TocWebApplication {
 
     public static void main(String[] args) {
--- toc-web/src/main/java/com/zyb/toc/web/controller/TaskController.java
+++ toc-web/src/main/java/com/zyb/toc/web/controller/TaskController.java
@@ -1,6 +1,10 @@
 package com.zyb.toc.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * javadoc TaskController
@@ -12,6 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @version 1.0.0
  **/
 @RestController
+@Slf4j
 public class TaskController {
 
+    @GetMapping(value = "/test")
+    public String test() throws InterruptedException {
+        log.error("test invoke");
+        Thread.sleep(TimeUnit.SECONDS.toMillis(5));
+        return "test";
+    }
 }
